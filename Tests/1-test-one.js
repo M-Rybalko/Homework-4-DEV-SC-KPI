f11983d13aaf10b091ede0708e5fbe1427e61932
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-02-13 21:34:19 +0200

    Added minor improvements

--- Tests/1-test-one.js
+++ Tests/1-test-one.js
@@ -1,23 +1,30 @@
 const test = require('../test.js');
-const fn = require('../Tasks/1-one.js');
+const task1 = require('../Tasks/1-one.js');
 
-const id = Symbol('id');
+const test1 = () => {
+  const id = Symbol('id');
 
-const cases = [
-  [
-    fn({
-      name: 'Alex',
-      age: 23,
-      [id]: 'id',
-    }), 2
-  ],
-  [
-    fn({
-      name: 'Alex',
-      age: 23,
-      id: 'id',
-    }), 3
-  ],
-]
+  const cases = [
+    [
+      task1({
+        name: 'Alex',
+        age: 23,
+        [id]: 'id',
+      }), 2
+    ],
+    [
+      task1({
+        name: 'Alex',
+        age: 23,
+        id: 'id',
+      }), 3
+    ],
+  ]
+  
+  console.log('Task 1')
+  test(cases);
+}
 
-test(cases);
+test1()
+
+module.exports = test1;
