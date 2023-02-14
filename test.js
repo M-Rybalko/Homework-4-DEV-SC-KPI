24bb3479a1404ba6f4643c3d404c7b988b163e0e
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-02-14 22:25:30 +0200

    Style fixes

--- test.js
+++ test.js
@@ -5,8 +5,10 @@ const test = (cases) => {
 
   const assert = (fn, res) => {
 
-    if (typeof fn === typeof res && 
-    JSON.stringify(fn) === JSON.stringify(res)) {
+    if (
+    typeof fn === typeof res && 
+    JSON.stringify(fn) === JSON.stringify(res)
+    ) {
       passed++;
     }
 
