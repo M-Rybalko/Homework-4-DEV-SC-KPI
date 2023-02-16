f18eb8dfa4d90045968b2d849c857d237bb4e286
Maksym Rybalko <maxymrybalko2005@gmail.com>
2023-02-16 20:19:18 +0200

    Added task 5

---
+++ Tasks/5-five.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const task5 = {
+
+  students: new Map(),
+  passwords: {
+
+  },
+
+  makeAccount(student) {
+    const {name, surname, grade, institute, security: {password, email}, ...subjects} = student;
+
+    const account = {
+      username: this.makeUsername(name, surname),
+      email: email,
+      averageMark: this.makeAverageMark(subjects),
+      subjects: subjects,
+    }
+
+    this.passwords[account.username] = password;
+    this.students.set(account.username, account);
+    return account;
+  },
+
+  makeUsername (name, surname) {
+    return name.concat(surname);
+  },
+
+  makeAverageMark (subjects) {
+    let average = 0;
+    const marks = Object.values(subjects);
+
+    for (const subject of marks ) {
+      average += subject;
+    }
+
+    return average / marks.length;
+  },
+
+  getMark (username, password) {
+
+    if (password !== this.passwords[username]) {
+      throw new Error('Wrong password and/or username');
+    }
+
+    const student = this.students.get(username);
+    return student.averageMark;
+  }
+
+}
+
+module.exports = task5;
